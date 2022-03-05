33f1615955f5ac42a33a1daf08946c522b6124e9
Vitor Rocha <vitor.league@gmail.com>
2022-03-05 01:29:36 -0300

    Removendo README

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,20 +1,19 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from 'react-router-dom';
-import Home from './Home/Home';
-import Login from './Login/Login';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import StoreProvider from "components/Store/Provider";
+import RoutesPrivate from "components/Routes/Private/Private";
 
 const PagesRoot = () => (
-  <Router>
-    <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/" component={Home} />
-    </Switch>
-  </Router>
-)
-
+  <StoreProvider>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <RoutesPrivate path="/" component={Home} />
+      </Switch>
+    </Router>
+  </StoreProvider>
+);
 
 export default PagesRoot;
